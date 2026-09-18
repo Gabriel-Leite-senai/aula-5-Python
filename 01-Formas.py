c05479cb18f0b19c6546f9c39451b782f35939b9
Gabriel-Leite-senai <gabriel.camandona@edu.senai.br>
2026-09-18 13:36:53 -0300

    First/Menu criado

---
+++ 01-Formas.py
@@ -0,0 +1,31 @@
+while True:
+
+    print("1 - Circulo")
+    print("2 - Triângulo")
+    print("3 - Quadrado")
+    print("4 - Retângulo")
+    print("5 - Paralelograma")
+    print("6 - Losango")
+    print("7 - Trapésio")
+    print("0 - Sair do Programa")
+
+    opcao = input("selecione a forma que você deseja calcular")
+
+    if opcao == "1":
+        
+    elif opcao == "2":
+
+    elif opcao == "3":
+
+    elif opcao == "4":
+        
+    elif opcao == "5":
+
+    elif opcao == "6":
+        
+    elif opcao == "7":
+        
+    elif opcao == "0"
+        break
+    else
+        print("Opção invalida")
